6688707e6d04d4324d738d1fb7820f97cfdb1557
Innocent Onyemaenu <innocent.onyemaenu@lixy.tech>
2021-09-05 21:05:30 +0100

    initial commit react-shopper

---
+++ src/Cart/Cart.tsx
@@ -0,0 +1,32 @@
+import CartItem from '../CartItem/CartItem';
+import { Wrapper } from './Cart.styles';
+import { CartItemType } from '../App';
+
+type Props = {
+  CartItems: CartItemType[];
+  addToCart: (clickedItem: CartItemType) => void;
+  removeFromCart: (id: number) => void;
+};
+
+const Cart: React.FC<Props> = ({ CartItems, addToCart, removeFromCart }) => {
+  const calculateTotal = (items: CartItemType[]) =>
+    items.reduce((ack: number, item) => ack + item.amount * item.price, 0);
+
+  return (
+    <Wrapper>
+      <h2>Your Shopping Cart</h2>
+      {CartItems.length ===0 ? <p>No items in cart.</p> : null}
+      {CartItems.map(item => (
+        <CartItem
+          key={item.id}
+          item={item}
+          addToCart={addToCart}
+          removeFromCart={removeFromCart}
+        />
+      ))}
+      <h2>Total: ${calculateTotal(CartItems).toFixed(2)}</h2>
+    </Wrapper>
+  );
+};
+
+export default Cart;
